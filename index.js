8e6943f965e4b93eb8a79e16de809a480c4e4069
Arley Prates <arleyprates@gmail.com>
2017-12-04 13:52:57 -0300

    Update

--- index.js
+++ index.js
@@ -1,33 +1,51 @@
 var express		=	require('express');
 var app			=	express();
 var mysql		=	require('mysql');
-var connection	=	mysql.createConnection({
-  host     : 'localhost',
-  port     : 3306,
-  user     : 'root',
-  password : '123',
-  database : 'nodejs'
-});
-var bodyParser 	=	require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+function execSQLQuery(sqlQry, res){
+  const connection = mysql.createConnection({
+    host     : 'localhost',
+    port     : 3306,
+    user     : 'root',
+    password : '123',
+    database : 'nodejs'
+  });
+
+  connection.query(sqlQry, function(error, results, fields){
+      if(error) 
+        res.json(error);
+      else
+        res.json(results);
+      connection.end();
+      console.log('executou!');
+  });
+}
 
 var router = express.Router();
 router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
 
+var bodyParser 	=	require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.set('port', process.env.PORT || 3000);
+
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
+
+app.set('view engine', 'ejs');
+
 app.use('/', router);
+
+app.get('/login', function(req, res) {
+    res.render('login');
+});
+app.get('/cadastro', function(req, res) {
+    res.render('cadastro');
+});
 
 app.listen(3000);
 console.log('API funcionando! server listening on port ' + app.get('port'));
 
-connection.connect(function(err){
-  if(err) return console.log(err);
-  console.log('DB conected!');
+router.get('/usuarios', (req, res) =>{
+    execSQLQuery('SELECT * FROM usuario', res);
 })
-
-/*connection.query("SELECT * FROM usuario", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-});*/
